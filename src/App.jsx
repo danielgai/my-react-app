666c603b9afb67c9b65c297a304793757432aaa5
Daniel <d.d.gai@wustl.edu>
2024-11-13 00:42:41 -0600

    Add 9 squares in single line, and provided styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,5 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return(
+  <>
+   <button className="square">X</button>
+   <button className="square">X</button>
+   <button className="square">X</button>
+   <button className="square">X</button>
+   <button className="square">X</button>
+   <button className="square">X</button>
+   <button className="square">X</button>
+   <button className="square">X</button>
+   <button className="square">X</button>
+  </>
+  );
 }

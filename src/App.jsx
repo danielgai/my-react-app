03c581951e6aab755f1bbac91f983c4e11dbff51
Daniel <d.d.gai@wustl.edu>
2024-11-13 00:45:37 -0600

    change name of component to Board

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-export default function Square() {
+export default function Board() {
   return(
     <>
     <div className="board-row">
